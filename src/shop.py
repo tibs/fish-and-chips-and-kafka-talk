f0c11477bec1af21a6f0ced38538bb7dd5cf1614
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2022-08-25 17:28:01 +0100

    Both Producer and Consumer in separate Panels
    This would appear to show me how to do all I need to be able to do for
    the demo (well, I'm not using any Redis, or sending to OpenSearch, etc.,
    but the Producer/Consumer are there).
    
    There's scope for a lot of refactoring <smile>

--- src/shop.py
+++ src/shop.py
@@ -9,6 +9,13 @@
 
 import asyncio
 import click
+import pathlib
+import json
+if True:
+    import aiokafka
+    import aiokafka.helpers
+else:
+    import kafka
 
 from collections import deque
 
@@ -25,6 +32,8 @@ from textual.widgets import Header, Footer, Placeholder, ScrollView
 
 TOPIC_NAME = 'fish-and-chips'
 
+global KAFKA_URI    # for the moment
+global CERTS_DIR    # for the moment
 
 class Clock(Widget):
     def on_mount(self):
@@ -53,34 +62,6 @@ class LineWidget(Widget):
         text = self.make_text(self.size.height)
         self.counter += 1
         return Panel(text)
-
-
-class MyGridApp(App):
-
-    async def on_load(self, event: events.Load) -> None:
-        """Bind keys with the app loads (but before entering application mode)"""
-        await self.bind("q", "quit", "Quit")
-
-    async def on_mount(self, event: events.Mount) -> None:
-        grid = await self.view.dock_grid(edge='left', name='left-grid')
-
-        grid.add_column('left', fraction=1, min_size=20)
-        grid.add_column('right', fraction=1, min_size=20)
-
-        grid.add_row('top', fraction=1)
-        grid.add_row('bottom')
-
-        grid.add_areas(
-            area1='left,top',
-            area2='right,top',
-            area3='left-start|right-end,bottom',
-        )
-
-        grid.place(
-            area1=Clock(),
-            area2=OtherWidget(),
-            area3=LineWidget(),
-        )
 
 
 class OtherWidget(Widget):
@@ -112,9 +93,183 @@ class OtherWidget(Widget):
         return Panel(text)
 
 
+class KafkaProducerWidget(Widget):
+
+    MAX_LINES = 30
+
+    count = 0
+    lines = deque(maxlen=MAX_LINES)
+
+    async def background_task(self):
+        try:
+            context = aiokafka.helpers.create_ssl_context(
+                cafile=CERTS_DIR / "ca.pem",
+                certfile=CERTS_DIR / "service.cert",
+                keyfile=CERTS_DIR / "service.key",
+            )
+        except Exception as e:
+            self.lines.append(f'Producer SSL Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Producer SSL context acquired')
+
+        try:
+            producer = aiokafka.AIOKafkaProducer(
+                bootstrap_servers=KAFKA_URI,
+                security_protocol="SSL",
+                ssl_context=context,
+                value_serializer=lambda v: json.dumps(v).encode('ascii'),
+            )
+        except Exception as e:
+            self.lines.append(f'Producer Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Producer created')
+
+        try:
+            await producer.start()
+        except Exception as e:
+            self.lines.append(f'Producer start Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Producer started')
+
+        try:
+            while True:
+                await asyncio.sleep(0.5)
+                self.count += 1
+                message = f'Message {self.count}'.encode('utf-8')
+                message = {
+                    'order': self.count,
+                    'data': {'cod': 1, 'chips': 1},
+                }
+
+                self.lines.append(f'Sending message {message!r}')
+                self.refresh()
+                self.app.refresh()
+
+                await producer.send_and_wait(TOPIC_NAME, message)
+        except Exception as e:
+            self.lines.append(f'Exception sending message {e}')
+            self.refresh()
+            self.app.refresh()
+        finally:
+            self.lines.append(f'Producer stopping')
+            self.refresh()
+            self.app.refresh()
+            await producer.stop()
+
+    async def on_mount(self):
+        asyncio.create_task(self.background_task())
+
+    def make_text(self, height):
+        lines = ['TOP'] + list(self.lines) + [f'BOTTOM height={height}']
+        # The value of 2 seems unnecessarily magical
+        # I assume it's the widget height - the panel border
+        return '\n'.join(lines[-(height-2):])
+
+    def render(self):
+        text = self.make_text(self.size.height)
+        return Panel(text, title='Producer')
+
+
+class KafkaConsumerWidget(Widget):
+
+    MAX_LINES = 30
+
+    count = 0
+    lines = deque(maxlen=MAX_LINES)
+
+    async def background_task(self):
+        try:
+            context = aiokafka.helpers.create_ssl_context(
+                cafile=CERTS_DIR / "ca.pem",
+                certfile=CERTS_DIR / "service.cert",
+                keyfile=CERTS_DIR / "service.key",
+            )
+        except Exception as e:
+            self.lines.append(f'Consumer SSL Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Consumer SSL context acquired')
+
+        try:
+            consumer = aiokafka.AIOKafkaConsumer(
+                TOPIC_NAME,
+                bootstrap_servers=KAFKA_URI,
+                security_protocol="SSL",
+                ssl_context=context,
+                value_deserializer = lambda v: json.loads(v.decode('ascii')),
+            )
+        except Exception as e:
+            self.lines.append(f'Consumer Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Consumer created')
+
+        try:
+            await consumer.start()
+        except Exception as e:
+            self.lines.append(f'Consumer start Exception {e.__class__.__name__} {e}')
+            return
+        self.lines.append('Consumer started')
+
+        try:
+            while True:
+                async for message in consumer:
+                    self.lines.append(f'Received {message.timestamp} : {message.value}')
+                    self.refresh()
+                    self.app.refresh()
+        except Exception as e:
+            self.lines.append(f'Exception receiving message {e}')
+            self.refresh()
+            self.app.refresh()
+            await producer.stop()
+        finally:
+            self.lines.append(f'Consumer stopping')
+            self.refresh()
+            self.app.refresh()
+            await consumer.stop()
+
+    async def on_mount(self):
+        asyncio.create_task(self.background_task())
+
+    def make_text(self, height):
+        lines = ['TOP'] + list(self.lines) + [f'BOTTOM height={height}']
+        # The value of 2 seems unnecessarily magical
+        # I assume it's the widget height - the panel border
+        return '\n'.join(lines[-(height-2):])
+
+    def render(self):
+        text = self.make_text(self.size.height)
+        return Panel(text, title="Consumer")
+
+
+class MyGridApp(App):
+
+    async def on_load(self, event: events.Load) -> None:
+        """Bind keys with the app loads (but before entering application mode)"""
+        await self.bind("q", "quit", "Quit")
+
+    async def on_mount(self, event: events.Mount) -> None:
+        grid = await self.view.dock_grid(edge='left', name='left-grid')
+
+        grid.add_column('left', fraction=1, min_size=20)
+        grid.add_column('right', fraction=1, min_size=20)
+
+        grid.add_row('top', fraction=1)
+        grid.add_row('bottom')
+
+        grid.add_areas(
+            area1='left,top',
+            area2='right,top',
+            area3='left-start|right-end,bottom',
+        )
+
+        grid.place(
+            #area1=Clock(),
+            area1=KafkaProducerWidget(),
+            area2=OtherWidget(),
+            area3=KafkaConsumerWidget(),
+        )
+
+
 @click.command(no_args_is_help=True)
-#@click.option('--go', required=True, is_flag=True, expose_value=False, help="Say --go to make the app run")
-##@click.argument('kafka_uri', required=True, help='Kafka service URI, as HOST:SSL_PORT')
 @click.argument('kafka_uri', required=True)
 @click.option('-d', '--certs-dir', default='creds',
               help='directory containing the ca.pem, service.cert and service.key files')
@@ -122,7 +277,11 @@ def main(kafka_uri, certs_dir):
     """A fish and chip shop demo, using Apache Kafka®
     """
 
+    global KAFKA_URI    # for the moment
+    global CERTS_DIR    # for the moment
     print(f'Kafka URI {kafka_uri}, certs dir {certs_dir}')
+    KAFKA_URI = kafka_uri
+    CERTS_DIR = pathlib.Path(certs_dir)
 
     MyGridApp.run(title="Simple App", log="textual.log")
 
